881a9118903a820a07ee66bcd28419e0308d62c0
Zach Smith <zachsmithglobal@icloud.com>
2024-02-29 22:02:12 -0500

    open access

--- server.js
+++ server.js
@@ -4,22 +4,6 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use((req, res, next) => {
-  let Referer = req.headers.referer;
-  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  if (ip === '::1' || ip === 'localhost' || ip === '127.0.0.1')  { // for local testing
-    next()
-  } else if (Referer) {
-    if (Referer.startsWith("https://sites.google.com/lcsnw.org/ourlcsnw/")) {
-      next();
-    } else {
-      res.status(403).send(`Access denied. This page must be accessed from the LCSNW intranet, you are accessing it from ${Referer}`);
-    }
-  } else {
-    res.status(403).send(`Access denied. This page must be accessed from the LCSNW intranet, you are accessing it from IP address: ${ip}`);
-  }
-});
-
 // Serve static files from both 'dist' and 'public' folders
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
