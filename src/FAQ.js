16a8262294bbb16012928a59493450da0c01ad81
Zach Smith <zachsmithglobal@icloud.com>
2024-03-01 15:39:53 -0500

    adds faq

--- src/FAQ.js
+++ src/FAQ.js
@@ -11,11 +11,48 @@ const FAQ = () => {
         {/* <h1>Frequently Asked Questions<br /></h1> */}
       </div>
       <div className='faqInfo'>
-        <h1>FAQ COMING SOON</h1>
-      {/* <h2>Existing Processes:</h2>
+        {/* <h1>FAQ</h1> */}
+      <h2>Concur Town Hall FAQ</h2>
         <p>
-          Q: How do I submit out of pocket costs?<br/>
-          Q: How do I code an invoice?<br/>
+          <strong>Q: What are some advantages of switching to KeyBank and the Concur expense management system?</strong><br/>
+          A: The KeyBank corporate card program adds value to LCSNW, as part of our overall banking partner conversion.  Concur Expense is a modern online workflow system with direct automated feed of agency credit card transactions, flexibility to pay out of pocket reimbursements twice a month and 24/7 support desk resources.<br/><br/>
+          <strong>Q: Do all LCSNW employees need to create an account in the Concur system?</strong><br/>
+          A: No. Only if you are an approved KeyBank MasterCard holder or have a legitimate out of pocket business expense requiring reimbursement by submitting an expense report.<br/><br/>
+          <strong>Q: How will out-of-pocket reimbursements be different in the new system?</strong><br/>
+          A: You may submit reimbursement requests twice a month, or continue to submit them once a month if you prefer. Expenses must be submitted within 60 days of the date incurred. Reimbursements will now be included as a separate line (not taxed, nor part of your earnings) on your 10th and 25th of the month paycheck based on timely submission and approval of your expense report.
+<br/><br/>
+          <strong>Q: How will entering vehicle mileage be different in the Concur system?</strong><br/>
+          A: You can use a mapping tool to automatically track mileage between waypoints or addresses. If you take a different route than what the map initially shows, you can “click” and “drag” the line to better represent the route(s) you actually drove. You can also use the “deduct commute” feature, which does the math for you – for instance, if your work requires driving to a different office than your normally assigned location.<br/><br/>
+          <strong>Q: Are mileage rates affected by the new system?</strong><br/>
+          A: No. Mileage rates are tied to Internal Revenue Service rates and automatically change when the IRS rate changes. The standard rate for 2024 is $0.67 cents per mile.<br/><br/>
+          <strong>Q: What is the purpose of the “add attendees” feature when submitting meal expenses?</strong><br/>
+          A: To meet federal tax requirements, you need to add the names of all the people who joined you for a meal and are included in the final bill. Attendees that were not found in the search feature can be entered manually and then will be saved for easy selection in the future.<br/><br/>
+          <strong>Q: What is the purpose of the “itemization” feature when submitting hotel expenses?</strong><br/>
+          A: Itemization ensures every type of expense is identified.  IRS regulations require Hotel bills to be itemized for room costs and verify any meals or incidental costs are separate and appropriately categorized by type, business purpose and if necessary, marked as personal (I.E. movie, partner meal, etc.)<br/><br/>
+          <strong>Q: What does it mean if I see an orange exclamation mark symbol when I’m submitting my expenses?</strong><br/>
+          A: These symbols are just “soft” reminders that help educate users as we get used to the new system and updated Travel and Expense (T&E) policy. You can still submit your expenses. <br/><br/>
+          <strong>Q: What does it mean if I see a red stop sign when I’m submitting my expenses?</strong><br/>
+          A: This brings a hard stop to the process. You have to resolve the identified problem in your expense report before your expenses can be submitted.<br/><br/>
+          <strong>Q: If I am an expense report approver, will I receive notification alerts like I did under the Wells Fargo system.</strong><br/>
+          A: Yes, if you have not turned off email alerts, approvers receive notification when a new report has entered their approval queue.  After 4-calendar days, if the report is still unapproved, another alert will be sent.  After 5 calendar days, the report will be escalated to the approvers manager.  Please review and approve submitted reports timely to ensure employees receive their reimbursements in their next paycheck.<br/><br/>
+          <strong>Q: If I am an expense report approver, how do I delegate someone to fill in for me when I am on vacation or otherwise unavailable?</strong><br/>
+          A: You no longer have to contact the expenses@lcsnw.org to assign a peer or above delegate.
+Click on the  Icon in the upper right hand corner and select “Personal Settings”
+Under Expense Settings, select Expense Delegates link to add your delegate<br/><br/>
+          <strong>Q: What should I do with my old Wells Fargo VISA cards after Feb. 29th? </strong><br/>
+          A: Cut them up or shred them.<br/><br/>
+          <strong>Q: When are expense reports due? </strong><br/>
+          A: Expense reports should be submitted on the 1st business day of the following month that  expenses are incurred. I.E. March “202403” expense report completed and submitted on April 1, 2024 allows for supervisor review and approval by April 3rd, then accounting review and posting to the accounting system (MIP GL) by April 5th and timely creation of monthly funder invoices and financial reporting.  Hint: adding expenses and uploading receipts throughout the month, makes the process smoother and easier to meet the timeline example above.
+<br/><br/>
+          <strong>Q: Are tolls a reimbursable expense? </strong><br/>
+          A: Yes<br/><br/>
+          <strong>Q: Are you able to upload multiple documents to support an expense?</strong><br/>
+          A: Yes<br/><br/>
+          <strong>Q: Are you able to restrict GL coding combinations entered (Program, RevSource, Area)? </strong><br/>
+          A: Not currently, but we are investigating ways to enhance the system to minimize coding errors.<br/>
+{/*
+          <strong>Q: How do I submit out of pocket costs?</strong><br/>
+          <strong>Q: How do I code an invoice?</strong><br/>
           Q: How do I make a check request?<br/>
           Q: How do I get a company card?<br/>
           Q: How do I submit an expense report?<br/>
@@ -47,8 +84,8 @@ const FAQ = () => {
           Banking treasury<br/>
           Banking merchant services<br/>
           Purchase cards and expense reporting<br/>
-          Credible matrix optimization<br/>
-        </p> */}
+          Credible matrix optimization<br/> */}
+        </p>
       </div>
     </div>
   );
