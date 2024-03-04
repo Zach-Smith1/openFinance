bdb1579de512779c299e49c560ad82a19ffef2a8
Zach Smith <zachsmithglobal@icloud.com>
2024-03-04 15:54:52 -0500

    cards and concur styling

--- src/CC.js
+++ src/CC.js
@@ -14,13 +14,14 @@ const CC = () => {
     <div className='CCPage'>
       <div className='CClinks'>
         <div className='thumbLink'>
+          <div className='overlay'>Travel and Expense Policy</div>
           <img
             src="../public/thumbs/TEthumb.jpg"
             alt="Thumbnail"
             onClick={() => thumbClick('../public/TEPolicy.pdf')}
             style={{ cursor: 'pointer', width: '300px' }}
-          /><br />
-          Travel and Expense Policy</div>
+          />
+          </div>
           <div className='thumbLink' id='video'>
           Concur Town Hall Video
         <video width="300" height="240" controls>
@@ -30,24 +31,25 @@ const CC = () => {
         </video><br />
           </div>
           <div className='thumbLink'>
+          <div className='overlay'>Concur Town Hall PowerPoint</div>
           <img
             src="../public/thumbs/ConcurTownThumb.jpg"
             alt="Thumbnail"
             onClick={() => thumbClick('../public/ConcurTownHall.pdf')}
             style={{ cursor: 'pointer', width: '300px' }}
-          /><br />
-          Concur Town Hall PowerPoint</div>
+          />
+          </div>
       </div>
       <h2>Bank Mastercard</h2><br />
       <div className='CClinks'>
-        <div className='thumbLink'><img
+        <div className='thumbLink'>
+        <div className='overlay'>Key Bank Commercial Card Activation Instructions</div>
+          <img
           src="../public/thumbs/keyComCard.jpg"
           alt="Thumbnail"
           onClick={() => thumbClick('../public/KeyCommercialCard.pdf')}
           style={{ cursor: 'pointer', width: '300px' }}
         />
-          <br />
-          Key Bank Commercial Card Activation Instructions
         </div>
         <div className='thumbLink' id='video'>
         KeyBank Card Activation Video
@@ -61,15 +63,16 @@ const CC = () => {
       <h2>Sap Concur Expense</h2>
       <div className='CClinks'>
         <div className='thumbLink'>
+        <div className='overlay'>QuickStart Concur Guide</div>
           <img
             src="../public/thumbs/quickStart.jpg"
             alt="Thumbnail"
             onClick={() => thumbClick('../public/ConcurExpenseGuide.pdf')}
             style={{ cursor: 'pointer', width: '300px' }}
-          /><br />
-          QuickStart Concur Guide</div>
+          />
+          </div>
           <div className='thumbLink' id='video'>
-          Sign into Concur with Two-Factor Authentication Video
+          Sign into Concur<br/>Two-Factor Authentication Video
           <video width="300" height="240" controls>
           {/* <source src="../public/ConcurMFAInstructions.mp4" type="video/mp4" /> */}
           <source src="https://github.com/Zach-Smith1/openFinance/raw/main/public/ConcurMFAInstructions.mp4" type="video/mp4" />
@@ -77,13 +80,13 @@ const CC = () => {
         </video><br />
           </div>
         <div className='thumbLink'>
+        <div className='overlay'>Concur Training Toolkit</div>
           <img
             src="../public/thumbs/concur.jpg"
             alt="Thumbnail"
             onClick={() => thumbClick('https://www.concurtraining.com/toolkit/standard/en/expense/end-user')}
             style={{ cursor: 'pointer', width: '300px' }}
-          /><br />
-          Concur Training Toolkit
+          />
         </div>
         <div><br />Note: To open links in a new tab in the PDF below, hold down the Command key on Mac or the Ctrl key on Windows when clicking links</div>
         <iframe id='pdf' src="../public/SAPConcurQuickStartChecklist.pdf" width="100%" height='800px'></iframe>
